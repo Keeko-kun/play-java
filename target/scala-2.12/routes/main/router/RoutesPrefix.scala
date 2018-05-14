8df05e87f195b215e7f2e46cfedd9e0c44a81d57
Gijs <g.vandenbiggelaar@gmail.com>
2018-05-14 10:36:56 +0200

    Initial commit

---
+++ target/scala-2.12/routes/main/router/RoutesPrefix.scala
@@ -0,0 +1,15 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:C:/Users/user/Documents/play-java/conf/routes
+// @DATE:Mon May 14 10:33:54 CEST 2018
+
+
+package router {
+  object RoutesPrefix {
+    private var _prefix: String = "/"
+    def setPrefix(p: String): Unit = {
+      _prefix = p
+    }
+    def prefix: String = _prefix
+    val byNamePrefix: Function0[String] = { () => prefix }
+  }
+}
